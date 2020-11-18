541a2b49bc741df283d1bc93330edaa113d2a733
Alexander Johnson <alexanderchristopherjohnson@gmail.com>
2020-11-18 11:10:12 -0800

    Update stopwatch.sv

--- stopwatch.sv
+++ stopwatch.sv
@@ -16,8 +16,6 @@ module stopwatch(
     output [3:0]an
     );
     
-    reg[3:0] d0,d1,d2,d3; //individual counts
-    reg[23:0] ticker; //24 bits to count to 10million (100Hz)
-    wire click;
+    
     
 endmodule
