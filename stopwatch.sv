1b368f381d7844a559ed570adb95b396e6e0b5e8
Alexander Johnson <alexanderchristopherjohnson@gmail.com>
2020-11-18 10:34:51 -0800

    Update stopwatch.sv

--- stopwatch.sv
+++ stopwatch.sv
@@ -1,28 +1,15 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Company: Cal Poly SLO
+// Engineer: Allison Lee and Alex Johnson
 // 
 // Create Date: 11/18/2020 10:26:32 AM
-// Design Name: 
-// Module Name: stopwatch
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
 //////////////////////////////////////////////////////////////////////////////////
 
 
 module stopwatch(
     input clk,
-    input stop,
+    input start_stop,
     input reset,
     output Seg,
     output decimal
