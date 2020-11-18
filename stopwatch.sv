a85c162c3cd3fae3e018f3d462348969da8b5632
Alexander Johnson <alexanderchristopherjohnson@gmail.com>
2020-11-18 10:29:54 -0800

    Add files via upload

---
+++ stopwatch.sv
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/18/2020 10:26:32 AM
+// Design Name: 
+// Module Name: stopwatch
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module stopwatch(
+    input clk,
+    input stop,
+    input reset,
+    output Seg,
+    output decimal
+    );
+endmodule
