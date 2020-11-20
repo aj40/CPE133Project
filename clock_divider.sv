995073184cac507756748f22b60db2c297f01cdf
aylee01 <72329229+aylee01@users.noreply.github.com>
2020-11-20 10:32:05 -0800

    updated clock divider

--- clock_divider.sv
+++ clock_divider.sv
@@ -10,17 +10,58 @@
 
 
 module clock_divider(
-    input clockin,
+    input clockin, reset,
     output clockout
-    );
+    ); 
     
-    parameter N = 17;
-    reg [N-1:0] count;
-    
-    always@(posedge clockin)
+    localparam N = 18;
+    reg [N-1:0]count;
+    reg [3:0] reg_d0, reg_d1, reg_d2, reg_d3;
+
+    always @ (posedge clockin or posedge reset)
     begin
-        count <= count + 1;
+        if (reset)
+            count <= 0;
+        else
+            count <= count + 1;
     end
+
+    reg [6:0]sseg;
+    reg [3:0]an_temp;
+    reg reg_dp;
+    always @ (*)
+    begin
+        case(count[N-1:N-2])
+            2'b00: 
+            begin
+                sseg = reg_d0;
+                an_temp = 4'b1110;
+                reg_dp = 1'b1;
+            end
+            
+            2'b01:
+            begin
+                sseg = reg_d1;
+                an_temp = 4'b1101;
+                reg_dp = 1'b0;
+            end
+   
+            2'b10:
+            begin
+                sseg = reg_d2;
+                an_temp = 4'b1011;
+                reg_dp = 1'b1;
+            end
     
-    assign clockout = count[N];   
+            2'b11:
+            begin
+                sseg = reg_d3;
+                an_temp = 4'b0111;
+                reg_dp = 1'b0;
+            end
+        endcase
+    end
+assign an = an_temp;
+     
 endmodule
+
