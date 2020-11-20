2592b80b80f966829117b128f273ac725c12688e
aylee01 <72329229+aylee01@users.noreply.github.com>
2020-11-20 10:43:47 -0800

    updated counter

--- counter.sv
+++ counter.sv
@@ -16,60 +16,58 @@ module counter(
     output reg d0,d1,d2,d3 
     );
     
-    reg[26:0] ticker; //27 bits (100MHz)
+    reg [26:0] ticks; //27 bits (100Hz)
     logic click;
     
-    always @ (posedge clock or posedge reset)
-begin
- if(reset)
+    always @ (posedge clk or posedge reset)
+        begin
+            if(reset)
+                ticks <= 0;
+            else if(ticks == 10000000) 
+                ticks <= 0;
+            else if(start_stop) 
+                ticks <= ticks + 1;
+        end
 
-  ticker <= 0;
+    assign click = ((ticks == 10000000)?1'b1:1'b0); 
 
-    else if(ticker == 10000000) 
-  ticker <= 0;
- else if(start) 
-  ticker <= ticker + 1;
-end
-
-    assign click = ((ticker == 10000000)?1'b1:1'b0); 
-
-always @ (posedge clk or posedge reset)
-begin
- if (reset)
-  begin
-    d0 <= 0;
-    d1 <= 0;
-    d2 <= 0;
-    d3 <= 0;
-  end
+    always @ (posedge clk or posedge reset)
+    begin
+        if (reset)
+        begin
+            d0 <= 0;
+            d1 <= 0;
+            d2 <= 0;
+            d3 <= 0;
+        end
   
- else if (click) 
-  begin
-   if(d0 == 9)
-   begin  
-     d0 <= 0;
+        else if (click) 
+        begin
+            if(d0 == 9)
+            begin  
+                d0 <= 0;
     
-    if (d1 == 9)  
-    begin  
-      d1 <= 0;
-     if (d2 == 5) 
-     begin 
-       d2 <= 0;
-      if(d3 == 9) 
-        d3 <= 0;
-      else
-        d3 <= d3 + 1;
-     end
-     else 
-      d2 <= d2 + 1;
-    end
+            if (d1 == 9)  
+            begin  
+                d1 <= 0;
+            if (d2 == 5) 
+            begin 
+                d2 <= 0;
+            if(d3 == 9) 
+                d3 <= 0;
+            else
+                d3 <= d3 + 1;
+            end
+            else 
+                d2 <= d2 + 1;
+        end
     
-    else 
-     d1 <= d1 + 1;
-   end 
+        else 
+            d1 <= d1 + 1;
+        end 
    
-   else 
-    d0 <= d0 + 1;
-  end 
-end
+        else 
+            d0 <= d0 + 1;
+        end
+    end  
 endmodule
