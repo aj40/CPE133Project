a8e5ae21695994537e34f196ce3433d905b9682f
Alexander Johnson <alexanderchristopherjohnson@gmail.com>
2020-11-20 10:48:39 -0800

    Update clock_divider.sv

--- clock_divider.sv
+++ clock_divider.sv
@@ -10,15 +10,17 @@
 
 
 module clock_divider(
-    input clockin, reset,
-    output clockout
+    input clk, reset,
+    input reg [3:0] d0,d1,d2,d3,
+    output [3:0] an,
+    output reg dp,
+    output [7:0] sseg
     ); 
     
     localparam N = 18;
     reg [N-1:0]count;
-    reg [3:0] reg_d0, reg_d1, reg_d2, reg_d3;
 
-    always @ (posedge clockin or posedge reset)
+    always @ (posedge clk or posedge reset)
     begin
         if (reset)
             count <= 0;
@@ -26,7 +28,6 @@ module clock_divider(
             count <= count + 1;
     end
 
-    reg [6:0]sseg;
     reg [3:0]an_temp;
     reg reg_dp;
     always @ (*)
@@ -34,34 +35,35 @@ module clock_divider(
         case(count[N-1:N-2])
             2'b00: 
             begin
-                sseg = reg_d0;
+                sseg = d0;
                 an_temp = 4'b1110;
                 reg_dp = 1'b1;
             end
             
             2'b01:
             begin
-                sseg = reg_d1;
+                sseg = d1;
                 an_temp = 4'b1101;
                 reg_dp = 1'b0;
             end
    
             2'b10:
             begin
-                sseg = reg_d2;
+                sseg = d2;
                 an_temp = 4'b1011;
                 reg_dp = 1'b1;
             end
     
             2'b11:
             begin
-                sseg = reg_d3;
+                sseg = d3;
                 an_temp = 4'b0111;
                 reg_dp = 1'b0;
             end
         endcase
     end
 assign an = an_temp;
+assign dp = reg_dp
      
 endmodule
 
